f5f17af7bf1ca58f6149b59b79ac8869e1c120cf
dimebruce <adriangarcia@qq.com>
2023-08-09 11:03:38 -0600

    Cambiando rutas y aplicando solucion de _redirects

--- src/Index.jsx
+++ src/Index.jsx
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom';
 const Index = () => {
   return (
     <div>
-      <h1>Este será el Index</h1>
-      <Link to="/coco">Ir a la página de perro</Link>
+      <h1 className='text-white'>Index</h1>
     </div>
   )
 }
