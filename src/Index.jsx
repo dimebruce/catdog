5b985617386d2039450be51b3f380ab6bcbbfca6
dimebruce <adriangarcia@qq.com>
2023-08-08 21:12:31 -0600

    Prueba de links en Netlify

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Index = () => {
   return (
     <div>
       <h1>Este será el Index</h1>
+      <Link to="/coco">Ir a la página de perro</Link>
     </div>
   )
 }
